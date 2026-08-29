3cec082de3a84e68fdfa980d9e0384208e0f5929
Niloy Chakma <niloychakma001@gmail.com>
2026-08-29 21:57:41 +0600

    added cashout form

--- home.js
+++ home.js
@@ -16,9 +16,34 @@ document.getElementById('btn-add-money')
         }
 
     })
+// Cashout calculation
+document.getElementById('btn-withdraw-money')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        const storedPin=1234;
+        const agentNumber=document.getElementById('agent-number').value;
+        const withdrawAddMoney = parseInt(document.getElementById('withdraw-add-money').value);
+        const withdrawPinNumber=Number(document.getElementById('withdraw-pin-number').value);
+        console.log(withdrawPinNumber,' ',withdrawAddMoney,' ',agentNumber.length);
+        let storedMoney = parseInt(document.getElementById('stored-money').innerText);
+        let totalMoney= storedMoney-withdrawAddMoney;
+        if(storedPin !==withdrawPinNumber || agentNumber.length!==11){
+            alert('Incorrect credentials');
+        }
+        else{
+            document.getElementById('stored-money').innerText=totalMoney;
+        }
+
+    })
     // toggle 
     document.getElementById('img-add-money')
     .addEventListener('click',function(){
         document.getElementById('form-add-money').style.display ='block';
-
+        document.getElementById('form-cashout').style.display ='none';
+    })
+// toggle - cashout 
+document.getElementById('img-cashout')
+    .addEventListener('click',function(){
+        document.getElementById('form-add-money').style.display ='none';
+        document.getElementById('form-cashout').style.display ='block';
     })
