ec6ce4024679c84a52da3280e724c31207d3da60
Niloy Chakma <niloychakma001@gmail.com>
2026-08-29 14:20:24 +0600

    added home page

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("btn-login")
     .addEventListener('click',function(event){
         event.preventDefault();
-        const inputNumber=parseInt(document.getElementById('input-number').value);
-        const inputPin=parseInt(document.getElementById('input-pin').value);
+        const inputNumber=number(document.getElementById('input-number').value);
+        const inputPin=number(document.getElementById('input-pin').value);
         console.log(inputNumber, inputPin);
         if(inputNumber===12345678 &&inputPin===1234){
             window.location.href="home.html";
