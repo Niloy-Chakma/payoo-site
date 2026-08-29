80ed8b2d7516a7567e30465d40c71cb92a4e65d5
Niloy Chakma <niloychakma001@gmail.com>
2026-08-29 22:06:23 +0600

    fixed some mistakes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("btn-login")
     .addEventListener('click',function(event){
         event.preventDefault();
-        const inputNumber=number(document.getElementById('input-number').value);
-        const inputPin=number(document.getElementById('input-pin').value);
+        const inputNumber=Number(document.getElementById('input-number').value);
+        const inputPin=Number(document.getElementById('input-pin').value);
         console.log(inputNumber, inputPin);
         if(inputNumber===12345678 &&inputPin===1234){
             window.location.href="home.html";
